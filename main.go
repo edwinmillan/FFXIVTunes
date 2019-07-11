a11e33e074f6ed6e7982ccd72f7938d342c20223
Edwin <bassgs3000@gmail.com>
2019-07-11 18:59:54 -0500

    Added features

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"golang.org/x/net/html"
@@ -29,8 +33,14 @@ func getHref(token html.Token) (ok bool, href string) {
 	return
 }
 
-func fetchPage(url string) (response *http.Response) {
-	var httpClient = &http.Client{Timeout: 10 * time.Second}
+func fetchPage(url string, isFile bool) (response *http.Response) {
+	// Assume the connection wont be open longer than the timeout period set
+	var httpClient = &http.Client{Timeout: 20 * time.Second}
+
+	if isFile {
+		httpClient.Timeout = 3 * time.Minute
+	}
+
 	response, err := httpClient.Get(url)
 	checkErr(err)
 
@@ -42,26 +52,30 @@ func parseFilename(url string) string {
 	return components[len(components)-1]
 }
 
-func downloadFile(filePath string, url string) error {
-	response := fetchPage(url)
+func downloadFile(filePath string, url string, waitGroup *sync.WaitGroup) {
+	fmt.Println("Downloading:", url)
+	response := fetchPage(url, true)
 	defer response.Body.Close()
 
 	outputFile, err := os.Create(filePath)
 	checkErr(err)
 	defer outputFile.Close()
 
-	// io.Copy keeps the buffer to 32kb. Keeping memory efficient.
+	// io.Copy keeps the buffer to 32kb. Keeping memory efficient. It also dumps the data in the file itself.
 	_, err = io.Copy(outputFile, response.Body)
-	return err
+	checkErr(err)
+
+	defer func() {
+		waitGroup.Done()
+		fmt.Println("File Saved to:", filePath)
+	}()
 }
 
-func getTunes() (songs []string) {
+func getTunes(expansion string) (songs []string) {
 	baseUrl := "https://ffxiv.tylian.net"
-	expansion := "ex3"
-
 	url := baseUrl + "/" + expansion
 
-	response := fetchPage(url)
+	response := fetchPage(url, false)
 	defer response.Body.Close()
 
 	tokenizer := html.NewTokenizer(response.Body)
@@ -91,15 +105,60 @@ func getTunes() (songs []string) {
 	}
 }
 
-func main() {
-	// TODO Coroutine downloading
-	// TODO Parametize expansion selection
-	// TODO Better Target Directory logic, aka dir creation
-	targetDir := "./output"
-
-	for _, songUrl := range getTunes() {
-		fmt.Println("Song URL:", songUrl)
-		fmt.Printf("\tFile Name:%s\n", parseFilename(songUrl))
-		downloadFile(targetDir+"/"+parseFilename(songUrl), songUrl)
+func getExpansion() (expansion string) {
+	expansions := map[int]string{
+		0: "quit",
+		1: "ffxiv",
+		2: "ex1",
+		3: "ex2",
+		4: "ex3",
 	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter the number of the expansion you want downloaded")
+	for key, val := range expansions {
+		fmt.Printf("[%d] %s\n", key, val)
+	}
+	fmt.Print("> ")
+	scanner.Scan()
+
+	if result, err := strconv.Atoi(strings.TrimSpace(scanner.Text())); err == nil {
+		expansion = expansions[result]
+		if expansion == "" {
+			fmt.Println("Try Again")
+			return getExpansion()
+		} else if expansion == "quit" {
+			os.Exit(0)
+		}
+	}
+
+	return
+}
+
+func main() {
+	var outputDir string
+
+	flag.StringVar(&outputDir, "o", "./output", "Directory for songs to be downloaded to")
+	flag.Parse()
+
+	expansion := getExpansion()
+	targetDir := strings.Join([]string{outputDir, expansion}, "/")
+
+	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+		fmt.Println("Creating", targetDir)
+		os.MkdirAll(targetDir, os.ModeDir)
+	}
+
+	songs := getTunes(expansion)
+	var waitGroup sync.WaitGroup
+
+	for _, songUrl := range songs {
+		waitGroup.Add(1)
+		filePath := strings.Join([]string{targetDir, parseFilename(songUrl)}, "/")
+		go downloadFile(filePath, songUrl, &waitGroup)
+	}
+
+	waitGroup.Wait()
+	fmt.Println("Completed!")
 }
